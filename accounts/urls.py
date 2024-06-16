84b710deab29ed7e260ea9a0bcbc76bad3fb1280
Ludmila Silveira <105131002+Ludmilahttps@users.noreply.github.com>
2024-06-15 21:45:27 -0300

    signin e signup frontend

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import profile
-from . import views
+from .views import signin_view, signup_view, profile
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('signin/', LoginView.as_view(template_name='accounts/signin.html'), name='signin'),
+    path('signin/', signin_view, name='signin'),  # URL para renderizar base.html com signin por padrão
+    path('signup/', signup_view, name='signup'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,14 @@ from django.contrib.auth import login, authenticate
 from .forms import SignUpForm
 from django.contrib.auth.decorators import login_required
 
-def signup(request):
+def signin_view(request):
+    context = {
+        'show_signin': True,
+        'show_signup': False
+    }
+    return render(request, 'accounts/sign.html', context)
+
+def signup_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -14,7 +21,12 @@ def signup(request):
             return redirect('home')
     else:
         form = SignUpForm()
-    return render(request, 'accounts/signup.html', {'form': form})
+    context = {
+        'show_signup': True,
+        'show_signin': False,
+        'form': form
+    }
+    return render(request, 'accounts/sign.html', context)
 
 @login_required
 def profile(request):
